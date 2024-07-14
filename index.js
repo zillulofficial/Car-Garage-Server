7ba8f80fef45c9265d732958df05e5f4f1076d0f
zillulofficial <zillul.hakim100@gmail.com>
2024-07-14 11:27:36 +0600

    local data stored in mongodb and accessed from server using get operation

--- index.js
+++ index.js
@@ -23,13 +23,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const serviceCollection= client.db('carGarage').collection('services')
+
+    app.get('/services', async(req, res)=>{
+      const cursor= serviceCollection.find()
+      const result= await cursor.toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
