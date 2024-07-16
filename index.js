c5875cbd4799134ae2cf8436b0383ceb7413528d
zillulofficial <zillul.hakim100@gmail.com>
2024-07-16 10:03:57 +0600

    find data by individual id has been functioned using get operation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app= express()
 const port= process.env.PORT || 5000
@@ -30,6 +30,13 @@ async function run() {
     app.get('/services', async(req, res)=>{
       const cursor= serviceCollection.find()
       const result= await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/services/:id', async(req, res)=>{
+      const id= req.params.id
+      const query= {_id: new ObjectId(id)} 
+      const result= await serviceCollection.findOne(query)
       res.send(result)
     })
 
