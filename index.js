3c6fa7860b6995841e3a74a82d6ac214aabc91dd
zillulofficial <zillul.hakim100@gmail.com>
2024-09-10 00:26:40 +0600

    custom middleware set with use of jwt and token verification

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app= express()
 const port= process.env.PORT || 5000
 
 // middleware
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.awpu5n8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,6 +26,23 @@ const client = new MongoClient(uri, {
   }
 });
 
+// custom middlewares
+const verifyToken= async(req, res, next)=>{
+  const token = req.cookies?.token
+  console.log("value inside token: ",token);
+  if(!token){
+    return res.status(401).send({message: 'Unauthorized'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({message: 'Unauthorized'})
+    }
+    console.log("Value in the token: ", decoded);
+    req.user= decoded
+    next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -28,6 +51,19 @@ async function run() {
     const serviceCollection= client.db('carGarage').collection('services')
     const bookingCollection= client.db('carGarage').collection('bookings')
 
+    // auth related API
+    app.post('/jwt', async(req,res)=>{
+      const user= req.body
+      const token =jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '3h'})
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+      })
+      .send({success: true})
+    })
+
+    // service related API
     app.get('/services', async(req, res)=>{
       const cursor= serviceCollection.find()
       const result= await cursor.toArray()
@@ -48,13 +84,32 @@ async function run() {
       const result= await bookingCollection.insertOne(booking)
       res.send(result)
     })
-    app.get('/bookings', async(req, res)=>{
+    app.get('/bookings',verifyToken, async(req, res)=>{
+      console.log(req.query.email);
+      // console.log(req.cookies.token);
+      console.log('user inside valid token: ',req.user );
+      if(req.query.email!== req.user.email){
+        return res.status(403).send({message: "Forbidden Access"})
+      }
       let query= {}
       if(req.query?.email){
         query= {email: req.query.email}
       }
       const cursor= bookingCollection.find(query)
       const result= await cursor.toArray()
+      res.send(result)
+    })
+    app.patch('/bookings/:id', async(req, res)=>{
+      const id= req.params.id
+      const filter= {_id: new ObjectId(id)} 
+      const updatedBooking = req.body
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+      };
+      const result= await bookingCollection.updateOne(filter, updateDoc)
       res.send(result)
     })
     app.delete('/bookings/:id', async(req, res)=>{
