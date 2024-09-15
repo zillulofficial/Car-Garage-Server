86a54701c07801c3706eb78e9a967e50c01ea757
zillulofficial <zillul.hakim100@gmail.com>
2024-09-15 11:22:48 +0600

    vercel config added

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const port= process.env.PORT || 5000
 
 // middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: [,
+    'https://car-garage-cfd09.web.app',
+    'https://car-garage-cfd09.firebaseapp.com'
+  ],
   credentials: true
 }))
 app.use(express.json())
